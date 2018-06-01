ee3d33551cf27378c1a8b2af081dea9faf2db9de
siriinnovations <say2mahesh1991@gmail.com>
2018-06-01 15:37:48 +0530

    My first commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layout.main')
+@section('content')
+  <div class="row">
+    <div class="col-lg-12">
+      <h1 class="page-header">Dashboard</h1>
+    </div>
+    <!-- /.col-lg-12 -->
+  </div>
+  <div class="row">
+@endsection
